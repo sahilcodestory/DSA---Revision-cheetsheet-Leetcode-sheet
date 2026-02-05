2caa6e4a2598d7e69b215e566a7f29df95a53052
sahil codes <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>
2026-02-05 23:45:25 +0530

    Pattern 1: Added full pyramid

--- Day0-Patterns/Patterns.cpp
+++ Day0-Patterns/Patterns.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+void fullpyramid(int n)
+{
+    // int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        // spaces first
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
 
-int main(){
-    cout<<"heloo"<<endl;
+int main()
+{
+    // fullpyramid()
+    fullpyramid(5);
 }
