0c89ca6ab859bbb13717f84381c9b486f3536cb7
sahil codes <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>
2026-02-06 23:30:50 +0530

    Pattern3: Diamond pattern added

--- Day0-Patterns/Patterns.cpp
+++ Day0-Patterns/Patterns.cpp
@@ -32,28 +32,28 @@ using namespace std;
 //     }
 // }
 
-void invertedFullDiamondPyramid(int n){
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n-i-1; j++){
-            cout<< " ";
-        }
-        for(int j =0; j<i+1; j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-     for(int i = n; i > 0; i--){
+// void invertedFullDiamondPyramid(int n){
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n-i-1; j++){
+//             cout<< " ";
+//         }
+//         for(int j =0; j<i+1; j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+//      for(int i = n; i > 0; i--){
      
-        for(int j = 0; j< n-i+1; j++){
-            cout<<" ";
-        }
-           for(int j = 0; j < i-1; j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
+//         for(int j = 0; j< n-i+1; j++){
+//             cout<<" ";
+//         }
+//            for(int j = 0; j < i-1; j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
 
-}
+// }
 
 int main()
 {
@@ -64,5 +64,5 @@ int main()
     // invertedFullPyramid(5);
 
     //Pattern3 : DiamondPattern()
-    invertedFullDiamondPyramid(5);
+    // invertedFullDiamondPyramid(5);
 }
